6ce97f6315a01f87902200379a0ebb0fee07f72c
HanenFourati <fouratihanen@gmail.com>
2020-04-17 18:14:35 +0100

    Updates

---
+++ precache-manifest.ae805b2ceda00a5778e0a47ae911f430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641d813c37719c3930b0cf71d084c174",
+    "url": "/ma3an/index.html"
+  },
+  {
+    "revision": "78a078f49417928a682d",
+    "url": "/ma3an/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c160aebaa66c3a8fa6e8",
+    "url": "/ma3an/static/js/2.fbb3f82f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ma3an/static/js/2.fbb3f82f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a078f49417928a682d",
+    "url": "/ma3an/static/js/main.eff172a4.chunk.js"
+  },
+  {
+    "revision": "8470e4d9db84b1eefc02",
+    "url": "/ma3an/static/js/runtime-main.c31fec7d.js"
+  }
+]);
